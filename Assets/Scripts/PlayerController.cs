f6acdb782e368378ac0a070a14f97b7019aa95d8
Philip Amwata <philip@philipamwata.net>
2019-05-06 00:26:05 +0200

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private Vector3 origin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        origin = new Vector3(0.0f, 1.0f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vMove = Input.GetAxis("Vertical");
+        float hMove = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(hMove, 0.0f, vMove);
+
+        if(transform.position.y < 0)
+        {
+            transform.position = origin;
+        }
+        else
+        {
+            rb.AddForce(movement);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CSC3020H-Unity3D-Advanced
