e1fd27d63f9b8c8a75b1bab90747df80609986b5
Philip Amwata <philip@philipamwata.net>
2019-05-19 19:55:21 +0200

    Weapon sound and visual effects added

---
+++ Assets/Scripts/Player Scripts/PlayerAxeSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAxeSound : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] axeSound;
+
+    void PlayWooshSound()
+    {
+        audioSource.clip = axeSound[Random.Range(0, axeSound.Length)];
+        audioSource.Play();
+    }
+
+
+} // class
---
+++ Assets/Scripts/Scene Scripts/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyModel;
+
+    private int count = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (count <= 20)
+        {
+            yield return new WaitForSeconds(Random.Range(5.0f, 15.0f));
+            Vector3 randPos = new Vector3(Random.Range(1.0f, 15.0f), 0.5f, Random.Range(1.0f, 15.0f));
+            Instantiate(enemyModel, randPos, Quaternion.identity);
+            count++;
+        }
+    }
+}
